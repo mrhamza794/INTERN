c957eee9a8fd0d271ffaef5e89fe24b7ade13884
mrhamza794 <hamzach794@gmail.com>
2024-08-26 12:32:03 +0500

    Added script tag for main.js in index.html

---
+++ Ashar/assets/JS/main.js
@@ -0,0 +1,28 @@
+const arr = [
+  ( {
+    id: 1,
+    name: "Ahmed",
+    fatherName: "Tariq",
+    cgpa: 3.5,
+    document:["cnic", "img", "result"],
+  },
+  {
+    id: 2,
+    name: "Ashar",
+    fatherName: "Javaid",
+    cgpa: 3.5,
+    document:["cnic", "img", "result"],
+
+
+  },
+  {
+    id: 3,
+    name: "Hamza",
+    fatherName: "Iftikhar",
+    cgpa: 3.5,
+    document:["https://res.cloudinary.com/dqfta8r5p/image/upload/v1724407992/b5ovsujdg2cbpn5o7ols.jpg", "https://res.cloudinary.com/dqfta8r5p/image/upload/v1724407992/b5ovsujdg2cbpn5o7ols.jpg", "https://res.cloudinary.com/dqfta8r5p/image/upload/v1724407992/b5ovsujdg2cbpn5o7ols.jpg"],
+
+  }),
+];
+
+console.log(arr.name)
